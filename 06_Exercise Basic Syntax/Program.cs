2e96fcdff98b7bd25b21ddfc21a228b2f246490e
Nasonti <ay.stoev@gmail.com>
2022-09-28 01:26:57 +0300

    Add a new solution: 05. Login
    05. Login

---
+++ 06_Exercise Basic Syntax/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _05._Login
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                char currCh = username[i];
+                //Console.WriteLine(currCh);
+
+                password += currCh;
+            }
+
+            int totalTries = 1;
+
+            string enteredPassword;
+             while((enteredPassword = Console.ReadLine()) != password)
+             {
+                
+                if(totalTries >=4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+
+                totalTries++;
+             }
+            if (totalTries < 4)
+            {
+                Console.WriteLine($"User {username} logged in.");
+
+            }
+        }
+    }
+}
